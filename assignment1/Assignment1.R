de0a3fd0992e2fb19c042550264e9526cf472275
Michelle VanTieghem <michelle.vantieghem@gmail.com>
2019-01-24 15:41:31 -0500

    updating lesson, ready for feedback

---
+++ assignment1/Assignment1.R
@@ -0,0 +1,11 @@
+# Assignment 1 is interactive, so you will receive live feedback as you learn to code.
+# To begin, make sure you are in the cu-psych-1420 R project.
+# Then copy these 3 lines of code into your console and follow the instructions.
+
+swirl::install_course_github(github_username = "mvantieghem", course_name = "cu-psych-1420")
+library(swirl)
+swirl() 
+
+# If you don't reach the end of the assignment before the end of lab, you can exit swirl by typing:
+bye() 
+# next time you log-in, use the same username and it will remember where you left off.
--- how_to_run_swirl.R
+++
@@ -1,7 +0,0 @@
-swirl::install_course_github(github_username = "mvantieghem", course_name = "cu-psych-1420")
-library(swirl)
-swirl() #in the folder directory that's downloaded, and then follow the instructions that appear at console
-
-# how to exit swirl
-bye() 
-# next time you log-in, use the same username and it will remember where you left off.
