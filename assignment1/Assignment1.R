1d2733ec48322d3c518a3027b29e2c2c99054a92
Michelle VanTieghem <michelle.vantieghem@gmail.com>
2019-01-24 16:34:52 -0500

    fixing error

--- assignment1/Assignment1.R
+++ assignment1/Assignment1.R
@@ -2,7 +2,7 @@
 # To begin, make sure you are in the cu-psych-1420 R project.
 # Then copy these lines of code into your console and follow the instructions.
 
-install("swirlify")
+install.packages("swirlify")
 swirlify::demo_lesson()
 
 # If you don't reach the end of the assignment before the end of lab, you can exit swirl by typing:
